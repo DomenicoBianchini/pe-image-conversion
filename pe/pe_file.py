642c70b3660308c27901a487dc0ce1e6a7954dee
Domenico Bianchini <d.bianchini1@studenti.uniba.it>
2026-06-27 18:40:54 +0200

    Updated project code

--- pe/pe_file.py
+++ pe/pe_file.py
@@ -37,6 +37,6 @@ class PEFile:
                 targetSize = ImageMappingStrategy.getWidth()
                 image = image.resize(targetSize, targetSize)
 
-            # salvataggio dell'immagine generata
-            outputPath = "output/" + os.path.splitext(fileName)[0] + ".png"
+            # salvataggio dell'immagine generata con le dimensioni finali nel nome
+            outputPath = "images/" + os.path.splitext(fileName)[0] + "_" + str(image.getWidth()) + "x" + str(image.getHeight()) + ".png"
             image.save(outputPath)
