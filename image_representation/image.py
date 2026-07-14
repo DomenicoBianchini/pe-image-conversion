6f101aa4be1fbdf8ba93c1b29224af3cea37e09f
Domenico Bianchini <d.bianchini1@studenti.uniba.it>
2026-07-14 16:01:18 +0200

    Updated code

--- image_representation/image.py
+++ image_representation/image.py
@@ -4,23 +4,23 @@ from PIL import Image as PILImage
 class Image:
 
     def __init__(self, data):
-        self.data = data
+        self.__data = data
 
     def getWidth(self):
-        return self.data.shape[1]
+        return self.__data.shape[1]
 
     def getHeight(self):
-        return self.data.shape[0]
+        return self.__data.shape[0]
 
     def getNumberOfChannels(self):
-        return self.data.shape[2]
+        return self.__data.shape[2]
 
     def save(self, fileName):
 
         # salvataggio dell'immagine in base al numero di canali
         if self.getNumberOfChannels() == 1:
-            image = PILImage.fromarray(self.data[:, :, 0], mode="L")
+            image = PILImage.fromarray(self.__data[:, :, 0], mode="L")
         else:
-            image = PILImage.fromarray(self.data, mode="RGB")
+            image = PILImage.fromarray(self.__data, mode="RGB")
 
         image.save(fileName)
